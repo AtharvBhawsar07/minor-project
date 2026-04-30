6235bc2d7d65df0002e2c78093dda51aa42d9e2b
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-30 21:35:28 +0530

    source changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://minor-project-4rrb.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
