f9a1a1d097808366752d9d91a13c15713b2386f8
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-30 22:43:56 +0530

    pushed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://digital-library-hehl.onrender.com ';
+const API_URL = 'https://digital-library-hehl.onrender.com/api ';
 
 const api = axios.create({
   baseURL: API_URL,
